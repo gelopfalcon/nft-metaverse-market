b5dee5aa84725e2ecb0e9472ae4cb45cf1d67f83
gelopfalcon <gelopfalcon@gmail.com>
2021-10-13 10:01:38 -0600

    Add base project

---
+++ test/sample-test.js
@@ -0,0 +1,48 @@
+describe("NFTGallery", function() {
+  it("Should create and execute market sales", async function() {
+    /* deploy the marketplace */
+    const Market = await ethers.getContractFactory("NFTGallery")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
+
+    /* deploy the NFT contract */
+    const NFT = await ethers.getContractFactory("NFToken")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    const auctionPrice = ethers.utils.parseUnits('1', 'ether')
+
+    /* create two tokens */
+    await nft.createNFToken("https://www.falcontoken.com")
+    await nft.createNFToken("https://www.costarica.com")
+
+    /* put both tokens for sale */
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice })
+
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    /* execute sale of token to another user */
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    /* query for and return the unsold items */
+    items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+  })
+})
